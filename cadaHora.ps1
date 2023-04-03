34da6956aa9068fba984e550a7ac761f28f412ae
Anselmo Estrada Roa <anselmoestradaroa@gmail.com>
2023-04-03 17:59:11 -0300

    Este script suena el archivo wav la cantidad de horas que corresponden

---
+++ cadaHora.ps1
@@ -0,0 +1,17 @@
+$sound = new-Object System.Media.SoundPlayer; 
+$sound.SoundLocation = "C:\Users\Antonio\Music\Sonidos\160483__koyber__casio-f-91w-hour-chime.wav";
+
+[int]$hora = Get-Date -Format "HH"
+
+
+if ($hora -le 12) {
+    for ($i = ($hora - 1) ; $i -ge 0; $i--) {
+        $sound.Play(); # If called using "powershell -file thisfile.ps1", you need to add a delay, # otherwise the script ends before the sound has played. 
+        Start-Sleep -s 1
+    }
+} else {
+    for ($i = ($hora - 1) ; $i -ge 12; $i--) {
+        $sound.Play(); # If called using "powershell -file thisfile.ps1", you need to add a delay, # otherwise the script ends before the sound has played. 
+        Start-Sleep -s 1
+    }
+}
